aecee6bb9422d89394ec7ea85f6255454f75b8eb
kiang <kiange@gmail.com>
2011-01-04 18:28:17 +0800

    Complete the newebmpp payment module

--- includes/modules/payment/newebmpp.php
+++ includes/modules/payment/newebmpp.php
@@ -1,28 +1,18 @@
 <?php
-
 /* -----------------------------------------------------------------------------------------
-  $Id: NewebmPP.php,v 1.1 2003/09/06 22:13:54 oldpa Exp $
-
-  TWE-Commerce - community made shopping
-  http://www.oldpa.com.tw
-  Copyright (c) 2003 TWE-Commerce
-  -----------------------------------------------------------------------------------------
   based on:
-  (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-  (c) 2002-2003 osCommerce(cod.php,v 1.28 2003/02/14); www.oscommerce.com
-  (c) 2003	 nextcommerce (cod.php,v 1.7 2003/08/24); www.nextcommerce.org
-  (c) 2003	 xt-commerce  www.xt-commerce.com
+  (c) TWE-Commerce http://www.oldpa.com.tw
   Released under the GNU General Public License
   --------------------------------------------------------------------------------------- */
 
-class NewebmPP {
+class newebmpp {
 
-    var $code, $title, $description, $enabled;
+    var $code, $title, $description, $enabled, $logPath;
 
     // class constructor
-    function NewebmPP() {
+    function newebmpp() {
         global $order;
-        $this->code = 'NewebmPP';
+        $this->code = 'newebmpp';
         $this->title = MODULE_PAYMENT_NEWEBMPP_TEXT_TITLE;
         $this->description = MODULE_PAYMENT_NEWEBMPP_TEXT_DESCRIPTION;
         $this->sort_order = MODULE_PAYMENT_NEWEBMPP_SORT_ORDER;
@@ -34,6 +24,10 @@ class NewebmPP {
         if (is_object($order))
             $this->update_status();
         $this->form_action_url = MODULE_PAYMENT_NEWEBMPP_TRANSURL;
+        $this->logPath = DIR_FS_CATALOG . '/log/newebmpp';
+        if(!file_exists($this->logPath)) {
+            mkdir($this->logPath, 0755, true);
+        }
     }
 
     // class methods
@@ -59,18 +53,21 @@ class NewebmPP {
     }
 
     function getOrderNumber() {
-        $Year = substr(date('Y'), 3);
-        $Day = date('z');
-        $Hour = date('H');
-        $Minute = date('i');
-        $Second = date('s');
-        $Sec = ($Hour * 60 + $Minute) * 60 + $Second;
-        return "$Year$Day$Sec";
+        $orderNumberCounter = $this->logPath . '/orderNumberCounter.txt';
+        if(file_exists($orderNumberCounter)) {
+            $orderNumber = filesize($orderNumberCounter) + 1;
+            $fh = fopen($orderNumberCounter, 'a');
+            fwrite($fh, '0');
+            fclose($fh);
+        } else {
+            $orderNumber = 1;
+            file_put_contents($orderNumberCounter, '0');
+        }
+        return $orderNumber;
     }
 
     function process_button() {
-        global $order, $SID;
-        $SID = xtc_session_name() . '=' . xtc_session_id();
+        global $order;
 
         $MerchantNumber = MODULE_PAYMENT_NEWEBMPP_MERCHANTNUMBER;
         $CheckSum = MODULE_PAYMENT_NEWEBMPP_CHECKSUM;
@@ -95,26 +92,39 @@ class NewebmPP {
     }
 
     function before_process() {
-        global $_POST;
+        global $order;
         $FinalResult = $_POST['final_result'];
         $PRC = $_POST['final_return_PRC'];
         $SRC = $_POST['final_return_SRC'];
         if ($FinalResult != "1") {
             xtc_redirect(xtc_href_link(FILENAME_CHECKOUT_PAYMENT, "error_message=" . urlencode("交易失敗! 回覆碼=(" . $_POST['final_return_PRC'] . "/" . $_POST['final_return_SRC'] . "/" . $_POST['final_return_BankRC'] . ")"), 'SSL', true, false));
         } else {
-            //-- 接收結果參數
+            /*
+             * Receive results
+             */
             $MerchantNumber = $_POST['P_MerchantNumber'];
             $OrderNumber = $_POST['P_OrderNumber'];
             $Amount = $_POST['P_Amount'];
             $CheckSum = $_POST['P_CheckSum'];
 
-            //交易結果成功，檢查結果是否遭竄改
+            /*
+             * Check the result
+             */
             $Code = MODULE_PAYMENT_NEWEBMPP_CHECKSUM;
             $checkstr = md5($MerchantNumber . $OrderNumber . $FinalResult . $PRC . $Code . $SRC . $Amount);
-            if ($checkstr != $CheckSum) {
+            if (($checkstr != $CheckSum) || (round($_POST['P_Amount']) != round($order->info['total']))) {
                 xtc_redirect(xtc_href_link(FILENAME_CHECKOUT_PAYMENT, "error_message=" . urlencode("交易失敗(檢查碼錯誤)!)"), 'SSL', true, false));
             } else {
-                //交易成功，請自行處理後續動作
+                /*
+                 * Check successfully
+                 */
+                $paymentLog = $this->logPath . '/' . date('Ym');
+                if(!file_exists($paymentLog)) {
+                    mkdir($paymentLog, 0755, true);
+                }
+                $paymentLog = $paymentLog . '/' . $OrderNumber;
+                file_put_contents($paymentLog, "{$_SERVER['REMOTE_ADDR']}\n" . print_r($_POST, true));
+                $_SESSION['orders_ident_key'] = $OrderNumber;
                 $ApproveCode = $_POST['final_return_ApproveCode'];
                 $BankRC = $_POST['final_return_BankRC'];
                 $BatchNumber = $_POST['final_return_BatchNumber'];
@@ -123,7 +133,17 @@ class NewebmPP {
     }
 
     function after_process() {
-        return false;
+        global $insert_id;
+        if (!empty($this->order_status)) {
+            xtc_db_query("UPDATE " . TABLE_ORDERS . "
+                SET orders_status='" . $this->order_status . "',
+                    orders_ident_key='" . $_SESSION['orders_ident_key'] . "'
+                    WHERE orders_id='" . $insert_id . "'");
+            xtc_db_query("UPDATE " . TABLE_ORDERS_STATUS_HISTORY . "
+                SET orders_status_id='" . $this->order_status . "'
+                    WHERE orders_id='" . $insert_id . "'");
+        }
+        unset($_SESSION['orders_ident_key']);
     }
 
     function get_error() {
@@ -146,7 +166,7 @@ class NewebmPP {
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_MERCHANTNUMBER', '201387','6', '1', now());");
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_CHECKSUM', 'abcd1234', '6', '1', now());");
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_SORT_ORDER', '0', '6', '0', now())");
-        xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_TRANSURL', 'https://maple.neweb.com.tw/NewebmPP/cdcard.jsp', '6', '0', now())");
+        xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_TRANSURL', 'https://maple.neweb.com.tw/newebmpp/cdcard.jsp', '6', '0', now())");
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_VERIFYURL', 'http://127.0.0.1/xt-Commerce/checkout_process.php', '6', '0', now())");
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_RETURNURL', 'http://192.168.1.5/xt-Commerce/checkout_process.php', '6', '0', now())");
         xtc_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_NEWEBMPP_ALLOWED', '',   '6', '0', now())");
